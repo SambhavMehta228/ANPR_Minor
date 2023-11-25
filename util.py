55e2a80a0329e2d22de139d9ae12fcea7429c903
SambhavMehta228 <115005827+SambhavMehta228@users.noreply.github.com>
2023-11-26 01:15:27 +0530

    Add files via upload

--- util.py
+++ util.py
@@ -1,10 +1,7 @@
 import string
 import os
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
-
 import easyocr
-
-
 
 # Initialize the OCR reader
 reader = easyocr.Reader(['en'], gpu=True)
@@ -26,7 +23,13 @@ dict_int_to_char = {'0': 'O',
 
 
 def write_csv(results, output_path):
-    
+    """
+    Write the results to a CSV file.
+
+    Args:
+        results (dict): Dictionary containing the results.
+        output_path (str): Path to the output CSV file.
+    """
     with open(output_path, 'w') as f:
         f.write('{},{},{},{},{},{},{}\n'.format('frame_nmr', 'car_id', 'car_bbox',
                                                 'license_plate_bbox', 'license_plate_bbox_score', 'license_number',
@@ -58,7 +61,16 @@ def write_csv(results, output_path):
 
 
 def license_complies_format(text):
-    if len(text) != 10:
+    """
+    Check if the license plate text complies with the required format.
+
+    Args:
+        text (str): License plate text.
+
+    Returns:
+        bool: True if the license plate complies with the format, False otherwise.
+    """
+    if len(text) != 7:
         return False
 
     if (text[0] in string.ascii_uppercase or text[0] in dict_int_to_char.keys()) and \
@@ -67,21 +79,26 @@ def license_complies_format(text):
        (text[3] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[3] in dict_char_to_int.keys()) and \
        (text[4] in string.ascii_uppercase or text[4] in dict_int_to_char.keys()) and \
        (text[5] in string.ascii_uppercase or text[5] in dict_int_to_char.keys()) and \
-       (text[6] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[2] in dict_char_to_int.keys()) and \
-       (text[7] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[2] in dict_char_to_int.keys()) and \
-       (text[8] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[2] in dict_char_to_int.keys()) and \
-       (text[9] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[2] in dict_char_to_int.keys()):
+       (text[6] in string.ascii_uppercase or text[6] in dict_int_to_char.keys()):
         return True
     else:
         return False
 
 
 def format_license(text):
-    
+    """
+    Format the license plate text by converting characters using the mapping dictionaries.
+
+    Args:
+        text (str): License plate text.
+
+    Returns:
+        str: Formatted license plate text.
+    """
     license_plate_ = ''
-    mapping = {0: dict_int_to_char, 1: dict_int_to_char, 4: dict_int_to_char, 5: dict_int_to_char, 6: dict_char_to_int,7: dict_char_to_int,8: dict_char_to_int,9: dict_char_to_int,
+    mapping = {0: dict_int_to_char, 1: dict_int_to_char, 4: dict_int_to_char, 5: dict_int_to_char, 6: dict_int_to_char,
                2: dict_char_to_int, 3: dict_char_to_int}
-    for j in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]:
+    for j in [0, 1, 2, 3, 4, 5, 6]:
         if text[j] in mapping[j].keys():
             license_plate_ += mapping[j][text[j]]
         else:
@@ -91,7 +108,15 @@ def format_license(text):
 
 
 def read_license_plate(license_plate_crop):
-   
+    """
+    Read the license plate text from the given cropped image.
+
+    Args:
+        license_plate_crop (PIL.Image.Image): Cropped image containing the license plate.
+
+    Returns:
+        tuple: Tuple containing the formatted license plate text and its confidence score.
+    """
 
     detections = reader.readtext(license_plate_crop)
 
@@ -107,7 +132,16 @@ def read_license_plate(license_plate_crop):
 
 
 def get_car(license_plate, vehicle_track_ids):
-   
+    """
+    Retrieve the vehicle coordinates and ID based on the license plate coordinates.
+
+    Args:
+        license_plate (tuple): Tuple containing the coordinates of the license plate (x1, y1, x2, y2, score, class_id).
+        vehicle_track_ids (list): List of vehicle track IDs and their corresponding coordinates.
+
+    Returns:
+        tuple: Tuple containing the vehicle coordinates (x1, y1, x2, y2) and ID.
+    """
     x1, y1, x2, y2, score, class_id = license_plate
 
     foundIt = False
